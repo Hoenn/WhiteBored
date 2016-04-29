d0719f908cd9a9d14fd01086c25bd4861d260da3
Hoenn <skatevan1313@gmail.com>
2016-04-29 14:18:07 -0400

    Finished animation for user change

--- public/script.js
+++ public/script.js
@@ -19,8 +19,10 @@ socket.on('new message', function(data){
 socket.on('users changed', function(numUsers){
   $('#userCount').text("Users: "+numUsers)
   $('#userCount').animate({
-    color: '#Ff000'
-  }, 2000);
+    color: '#000'
+  }, 500).animate({
+    color: '#f0f0f0'
+  },500);
 });
 
 //Listen to submit and generate message
